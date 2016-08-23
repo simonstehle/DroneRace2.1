f0bfd6823066306c9a26ea4ff4fb61fe3e8b739e
simonstehle <simonstehle@icloud.com>
2016-08-23 15:44:48 +0200

    Level one starting

--- js/level01.js
+++ js/level01.js
@@ -3,19 +3,33 @@
  */
 
 
+addStartFinishLine(1000,100,-4000,-80,0,0,800);
+
+
+addTube(25,200,250,1500,0,-7000, Math.PI * 0.3);
+
+build3WallObstacle(1000,2000,6000,-4000, Math.PI*0.2);
+
+addTargetRing(200, 250, 6000, 200, 0,0, scene);
+addTargetRing(200, 250, 5500, 200, 800,0, scene);
+addTargetRing(200, 250, 6100, 200, 1600,0, scene);
+
+
+
+
+
 
 addTargetRing(200, 250, 250, 250, 0,Math.PI*.25, scene);
-addTargetRing(2000, 2500, 2500, 2500, 1000,0, scene);
 
-build3WallObstacle(1000,2000,3000,1500, Math.PI * 0.25);
+
+
 
 
 
 addTargetFrame(800,-1000,2000,-2000, 0);
 
-addTube(25,200,250,-2000,0,-1000, Math.PI * 0.3);
 
 
 
 
-addStartFinishLine(1000,100,-4000,-80,0,0,800);
+
