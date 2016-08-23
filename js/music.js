06807bab8243f0fad133805f08d6c4946e8c67d3
simonstehle <simonstehle@icloud.com>
2016-08-23 16:22:44 +0200

    MUSIC

---
+++ js/music.js
@@ -0,0 +1,19 @@
+/**
+ * Created by simonstehle on 23.08.16.
+ */
+/**
+ *Find Docs here
+ * https://github.com/goldfire/howler.js
+ */
+
+var sound = new Howl({
+    src: ['objects/everything_is_awesome.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 0.5,
+    onend: function() {
+        console.log('Finished!');
+    }
+});
+
+sound.play();
